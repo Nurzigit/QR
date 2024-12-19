4653e0a378596770f8a9d14d4863541b7dcede6a
Nurik <nurzhigit.turman@narxoz.kz>
2024-12-20 01:47:42 +0500

    fix bags

--- src/Components/QRGenerator/index.jsx
+++ src/Components/QRGenerator/index.jsx
@@ -5,7 +5,6 @@ const QRGenerator = ({ queueNumber }) => (
   <div style={{ textAlign: "center", marginTop: "20px" }}>
     <h2>QR-код для номера очереди</h2>
     <QRCodeCanvas value={queueNumber.toString()} size={256} />
-    <p>Номер: {queueNumber}</p>
   </div>
 );
 
